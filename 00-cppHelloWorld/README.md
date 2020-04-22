16fe040d7a0188e21244f5b493812a71d50717b9
Nico Falchi <nicofalchi@gmail.com>
2020-04-22 20:45:12 -0300

    commint

---
+++ 00-cppHelloWorld/README.md
@@ -0,0 +1,8 @@
+# AED 
+ 
+* Algoritmos y Estructura de Datos
+* K1051
+* 2020
+* Numero de Legajo 1757878
+* Falchi 
+* Nicolas Alcides Alejandro
---
+++ 00-cppHelloWorld/hello.cpp
@@ -0,0 +1,9 @@
+  /*
+   Hello World
+  Nicolas Falchi
+   20200422
+   */
+   #include<iostream>
+   int main(){
+       std::cout <<"Hello World!\n";
+   }
