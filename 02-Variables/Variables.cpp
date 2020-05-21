8206240306441672a0148e1a642c5da0ddb2e127
Nico Falchi <nicofalchi@gmail.com>
2020-05-21 10:55:22 -0300

    commint

--- 02-Variables/Variables.cpp
+++ 02-Variables/Variables.cpp
@@ -4,7 +4,7 @@ int main() {
    float a;
    a =5.557889; 
    assert(a =5.557889);
-   unsigned b = -5000000000;
+   unsigned int b = -5000000000;
    assert(b==-5000000000);
    char i= 0011;
    assert(i==0011);
@@ -14,7 +14,7 @@ int main() {
    long l;
    l= 2123489;
    assert(l==2123489);
-   signed r = 15975345600000000;
+   signed int r = 15975345600000000;
    assert(r==15975345600000000);   
 return 0;
 
