4f4977cf1cd79fa2ac658916e5e402f371fd9734
Nico Falchi <nicofalchi@gmail.com>
2020-05-21 10:53:12 -0300

    commint

---
+++ 02-Variables/Variables.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+
+int main() {
+   float a;
+   a =5.557889; 
+   assert(a =5.557889);
+   unsigned b = -5000000000;
+   assert(b==-5000000000);
+   char i= 0011;
+   assert(i==0011);
+   short s;
+   s=256;
+   assert(s==256);
+   long l;
+   l= 2123489;
+   assert(l==2123489);
+   signed r = 15975345600000000;
+   assert(r==15975345600000000);   
+return 0;
+
+
+    
+   }
